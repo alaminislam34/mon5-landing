5b305142daf424cc9b36befb28181820a5c87e91
alaminislam34 <mi3548514@gmail.com>
2025-12-26 00:04:39 +0600

    update navbar design and added dropdown box

--- src/app/components/Header/Navbar.jsx
+++ src/app/components/Header/Navbar.jsx
@@ -1,31 +1,61 @@
 "use client";
 
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { AiFillMail } from "react-icons/ai";
 import { BsArrowUpRightSquareFill } from "react-icons/bs";
 
+const links = [
+  {
+    href: "/",
+    path: "Home",
+  },
+  {
+    href: "/about",
+    path: "About Us",
+  },
+  {
+    href: "/contact",
+    path: "Contact Us",
+  },
+];
+
 function Navbar() {
   const [isScroll, setIsScroll] = useState(false);
+  const pathName = usePathname();
+  const [showMenu, setShowMenu] = useState(false);
+  const [hasMounted, setHasMounted] = useState(false);
 
   useEffect(() => {
+    setHasMounted(true);
+
     const handleScroll = () => {
       setIsScroll(window.scrollY > 10);
     };
 
     window.addEventListener("scroll", handleScroll);
-
     handleScroll();
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  useEffect(() => {
+    setShowMenu(false);
+  }, [pathName]);
+
+  const navBackground =
+    hasMounted && isScroll
+      ? "bg-dark shadow-md shadow-white/10"
+      : "bg-transparent";
+
   return (
     <nav
-      className={`fixed top-0 left-0 w-full z-50 ${isScroll ? "bg-white" : ""}`}
+      className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${navBackground}`}
     >
       <section className="max-w-380 mx-auto px-4 py-3 flex flex-row items-center justify-between">
         <div>
@@ -34,32 +64,24 @@ function Navbar() {
             height={200}
             width={200}
             alt="Logo"
+            priority // Helps with LCP performance
             className="h-12 md:h-14 w-auto bg-cover bg-center object-cover"
           />
         </div>
         <div className="hidden lg:block">
           <ul className="flex flex-row items-center gap-6 ">
-            <li>
-              <Link className="py-2 px-6 rounded-lg bg-primary1" href={"/"}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="py-2 px-6 rounded-lg bg-primary1"
-                href={"/about"}
-              >
-                About Us
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="py-2 px-6 rounded-lg bg-primary1"
-                href={"/contact"}
-              >
-                Contact Us
-              </Link>
-            </li>
+            {links.map(({ href, path }) => (
+              <li key={href}>
+                <Link
+                  href={href}
+                  className={`py-2 px-6 rounded-lg ${
+                    pathName === href ? "bg-primary1 font-medium" : ""
+                  }`}
+                >
+                  {path}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
         <div className="hidden lg:flex items-center gap-4">
@@ -79,11 +101,63 @@ function Navbar() {
           </div>
         </div>
         <div className="lg:hidden">
-          <button className="py-2 px-4 rounded-lg bg-primary1 text-white">
-            <Menu />
+          <button
+            onClick={() => setShowMenu(!showMenu)}
+            className="py-2 px-3 rounded-lg bg-primary2 text-white"
+          >
+            {showMenu ? <X /> : <Menu />}
           </button>
         </div>
-        <div className="absolute top-16 right-6 border border-primary1 bg-white h-100 w-sm hidden"></div>
+
+        {/* dropdown */}
+        <div
+          className={`absolute duration-300 ${
+            showMenu ? " top-16" : "top-24 opacity-0 pointer-events-none"
+          } right-1 md:right-6 border border-primary2/20 bg-dark/80 backdrop-blur-2xl min-h-100 max-w-80 rounded-xl shadow-xl w-full`}
+        >
+          <div className="p-2 pt-4">
+            <Image
+              src={"/logos/logo.png"}
+              height={200}
+              width={200}
+              alt="Logo"
+              className="h-10 md:h-12 w-auto bg-cover bg-center object-cover"
+            />
+          </div>
+          <div className="border-b border-primary2/20 my-4"></div>
+          <ul className="flex flex-col gap-4 p-4">
+            {links.map(({ href, path }) => (
+              <li key={href}>
+                <Link
+                  href={href}
+                  className={`py-2 px-4 rounded-lg inline-block w-full duration-300 ${
+                    pathName === href
+                      ? "bg-primary2"
+                      : "hover:bg-primary1/50 border-b border-primary1/20"
+                  }`}
+                >
+                  {path}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <div className="flex flex-col gap-4 p-4 pb-6">
+            <div className="relative">
+              <AiFillMail className="absolute top-1/2 -translate-y-1/2 left-3 text-primary1" />
+              <input
+                type="text"
+                placeholder="Enter your mail"
+                className="py-2 pl-8 pr-4 rounded-lg ring-primary1/50 text-sm focus:outline-none focus:ring-primary2 ring-1 w-full"
+              />
+            </div>
+            <div>
+              <button className="py-2 px-4 rounded-lg bg-linear-to-r from-primary1 to-primary2 text-white font-semibold text-sm flex items-center flex-row gap-2 w-full">
+                Get Download Link
+                <BsArrowUpRightSquareFill />
+              </button>
+            </div>
+          </div>
+        </div>
       </section>
     </nav>
   );
--- src/app/components/Home/Banner/Banner.jsx
+++ src/app/components/Home/Banner/Banner.jsx
@@ -1,60 +1,95 @@
 import { TypeAnimation } from "react-type-animation";
 import Image from "next/image";
+import CountUp from "react-countup";
 
 function Banner() {
   return (
-    <div className="max-w-380 mx-auto min-h-screen bg-cover bg-center">
-      <section className="pt-16 flex items-center">
-        <div className="flex items-center justify-start min-h-[80vh] w-2/5 xl:w-2/6 px-4">
-          <div className="space-y-6 z-50">
-            <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-[1.2]">
-              Become the <br /> Ultimate{" "}
-              <span className="text-primary1 min-h-[1.2em]">
-                <TypeAnimation
-                  sequence={["Fantasy Coach", 1000, "", 1000]}
-                  speed={50}
-                  cursor={true}
-                  repeat={Infinity}
-                  style={{
-                    whiteSpace: "wrap",
-                  }}
+    <section className="relative">
+      <div className="max-w-380 mx-auto md:min-h-screen bg-cover bg-center z-10 relative">
+        <section className="flex items-center mt-20 min-h-[50vh] md:min-h-[50vh] lg:min-h-[65vh]">
+          <div className="flex items-center justify-start h-full md:pt-6 lg:pt-12 px-4">
+            <div className="space-y-8 z-50">
+              <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-[1.2]">
+                Become the <br /> Ultimate{" "}
+                <span className="text-primary1 min-h-[1.2em]">
+                  <TypeAnimation
+                    sequence={["Fantasy Coach", 1000, "", 1000]}
+                    speed={50}
+                    cursor={true}
+                    repeat={Infinity}
+                    style={{
+                      whiteSpace: "wrap",
+                    }}
+                  />
+                </span>
+              </h1>
+              <p className="sm:text-lg md:text-xl text-base max-w-[90%] md:max-w-[75%]">
+                Create your dream team, compete with friends, and track
+                real-time NBA stats. Ready to play?
+              </p>
+              <div className="flex flex-row items-center gap-4 sm:gap-6">
+                <Image
+                  src={"/images/google-play.png"}
+                  height={200}
+                  width={500}
+                  alt="Google play"
+                  className="h-10 sm:h-14 w-auto cursor-pointer hover:opacity-80 transition-opacity"
                 />
-              </span>
-            </h1>
-            <p className="sm:text-lg md:text-xl text-base max-w-[90%] md:max-w-[75%]">
-              Create your dream team, compete with friends, and track real-time
-              NBA stats. Ready to play?
-            </p>
-            <div className="flex flex-row items-center gap-4 sm:gap-6">
-              <Image
-                src={"/images/google-play.png"}
-                height={200}
-                width={500}
-                alt="Google play"
-                className="h-10 sm:h-14 w-auto cursor-pointer hover:opacity-80 transition-opacity"
-              />
-              <Image
-                src={"/images/app-store.png"}
-                height={200}
-                width={500}
-                alt="App Store"
-                className="h-10 sm:h-14 w-auto cursor-pointer hover:opacity-80 transition-opacity"
-              />
+                <Image
+                  src={"/images/app-store.png"}
+                  height={200}
+                  width={500}
+                  alt="App Store"
+                  className="h-10 sm:h-14 w-auto cursor-pointer hover:opacity-80 transition-opacity"
+                />
+              </div>
             </div>
           </div>
+        </section>
+        <div className=" w-full grid grid-cols-2 md:grid-cols-4 gap-4 items-center lg:mt-24">
+          <div className="flex flex-col gap-2 items-center text-[#cccccc]">
+            <h1 className="text-3xl lg:text-4xl xl:text-6xl font-bold">
+              <CountUp end={5} duration={2} />
+              k+
+            </h1>
+            <p className="md:text-lg lg:text-xl xl:text-2xl">Downloads</p>
+          </div>
+          <div className="flex flex-col gap-2 items-center text-[#cccccc]">
+            <h1 className="text-3xl lg:text-4xl xl:text-6xl font-bold">
+              <CountUp end={500} duration={3} />
+              k+
+            </h1>
+            <p className="md:text-lg lg:text-xl xl:text-2xl">Leagues Created</p>
+          </div>
+          <div className="flex flex-col gap-2 items-center text-[#cccccc]">
+            <h1 className="text-3xl lg:text-4xl xl:text-6xl font-bold">
+              <CountUp end={10} duration={2} />
+              k+
+            </h1>
+            <p className="md:text-lg lg:text-xl xl:text-2xl">
+              Pre-Registrations
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 items-center text-[#cccccc]">
+            <h1 className="text-3xl lg:text-4xl xl:text-6xl font-bold">
+              <CountUp end={1} duration={2} />
+              k+
+            </h1>
+            <p className="md:text-lg lg:text-xl xl:text-2xl">Active Players</p>
+          </div>
         </div>
-        <div className="border w-3/5 xl:w-4/6">
-          <Image
-            src={"/images/hand.png"}
-            width={1500}
-            height={1000}
-            alt="Hand image"
-            className="opacity-30 lg:opacity-100 w-full relative border border-white"
-            priority
-          />
-        </div>
-      </section>
-    </div>
+      </div>
+      <div className="absolute right-0 top-0 lg:-top-20 z-0">
+        <Image
+          src={"/images/hand.png"}
+          width={1500}
+          height={800}
+          alt="Hand image"
+          className="opacity-50 lg:opacity-100 w-full lg:max-h-50 xl:max-h-190"
+          priority
+        />
+      </div>
+    </section>
   );
 }
 
